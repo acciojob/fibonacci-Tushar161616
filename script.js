4f266fc431ece2cac1109de57635d2db00a41847
Tushar Sharma <145897973+Tushar161616@users.noreply.github.com>
2025-07-21 22:49:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
 function fibonacci(num) {
 // your code here
+	
+        let n1 = 0, n2 = 1;
+
+        for (let i = 0; i < num; i++) {
+            // Print the number
+            alert(n1 + " ");
+
+            // Swap
+            int n3 = n2 + n1;
+            n1 = n2;
+            n2 = n3;
+        }
+
 }
 
 module.exports = fibonacci;
